3ad8003218a9833a223c9d422acec8c0084ce5e7
Lukas Pedersen <lukas@agilex.dk>
2025-04-11 15:25:17 +0200

    colors -wip

--- resources/views/colors.blade.php
+++ resources/views/colors.blade.php
@@ -1,3 +1,10 @@
 <x-layout>
-
+    <div class="grid grid-cols-3 gap-4">
+        <div class="bg-light">
+            <p class="text-dark text-center p-2">light</p>
+        </div>
+        <div class="bg-primary">
+            <p class="text-white text-center p-2">primary</p>
+        </div>
+    </div>
 </x-layout>
