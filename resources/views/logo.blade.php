3317a38455b87aad89fb2b629f14662eef02ad54
Lukas Pedersen <lukas@agilex.dk>
2025-04-14 14:05:42 +0200

    feat: logo examples

---
+++ resources/views/logo.blade.php
@@ -0,0 +1,41 @@
+<x-layout>
+        <div class="grid gap-4">
+            <div class="grid grid-cols-3 gap-4">
+                <div class="border border-black p-4">
+                    <img src="{{ asset('images/logos/primary-secondary.svg') }}" alt="Primær Logo">
+                </div>
+                <div class="bg-secondary p-4">
+                    <img src="{{ asset('images/logos/primary-white.svg') }}" alt="Primær Logo">
+                </div>
+                <div class="bg-primary p-4">
+                    <img src="{{ asset('images/logos/secondary-white.svg') }}" alt="Primær Logo">
+                </div>
+                <div class="bg-black p-4">
+                    <img src="{{ asset('images/logos/white.svg') }}" alt="Primær Logo">
+                </div>
+                <div class="bg-secondary p-4">
+                    <img src="{{ asset('images/logos/primary.svg') }}" alt="Primær Logo">
+                </div>
+                <div class="bg-primary p-4">
+                    <img src="{{ asset('images/logos/secondary.svg') }}" alt="Primær Logo">
+                </div>
+            </div>
+            <div class="grid grid-cols-5 gap-4">
+                <div class="border border-black p-4 flex justify-center">
+                    <img src="{{ asset('images/icons/primary-secondary.svg') }}" alt="Primær Logo">
+                </div>
+                <div class="border border-black p-4 flex justify-center">
+                    <img src="{{ asset('images/icons/secondary-primary.svg') }}" alt="Primær Logo">
+                </div>
+                <div class="bg-secondary p-4 flex justify-center">
+                    <img src="{{ asset('images/icons/primary.svg') }}" alt="Primær Logo">
+                </div>
+                <div class="bg-primary p-4 flex justify-center">
+                    <img src="{{ asset('images/icons/secondary.svg') }}" alt="Primær Logo">
+                </div>
+                <div class="bg-black p-4 flex justify-center">
+                    <img src="{{ asset('images/icons/white.svg') }}" alt="Primær Logo">
+                </div>
+            </div>
+        </div>
+</x-layout>
