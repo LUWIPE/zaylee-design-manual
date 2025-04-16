be86de84257c17e8b3707090ba88dc66ce009fd2
Lukas <lukaswp2710@gmail.com>
2025-04-16 13:29:25 +0200

    feat: buttons examples

---
+++ resources/views/components/expamples/buttons/button-filled.blade.php
@@ -0,0 +1,6 @@
+<button
+    class="button bg-{{ $color }} hover:bg-{{ $color }}-dark border-2 border-{{ $color }} hover:border-{{ $color }}-dark text-{{ $text ?? 'white' }} h-12 rounded-lg px-5 py-2 button-press-feedback"
+    type="{{ $type ?? 'button' }}"
+>
+    Knap
+</button>
