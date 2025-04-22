3c8149873f2b7e228b1aaeb9bd4bc94001c587b9
Lukas Pedersen <lukas@agilex.dk>
2025-04-22 09:14:29 +0200

    feat: import Alpine JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,8 @@
 import './bootstrap';
+import Alpine from 'alpinejs';
 
 const currentPage = document.body.dataset.currentPage;
 // console.log(currentPage); // Uncomment to see the name of the current page.
+
+window.Alpine = Alpine;
+Alpine.start();
