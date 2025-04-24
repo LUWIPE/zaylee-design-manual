e469731c48103e0e9def029e4c5ec5ef16b64004
Lukas <lukaswp2710@gmail.com>
2025-04-24 11:51:59 +0200

    feat: linka text

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -1,3 +1,49 @@
 <x-layout>
-
+    <div class="grid gap-4">
+        <div>
+            <h1>Links</h1>
+        </div>
+        <div class="grid grid-cols-3 gap-4">
+            <x-expamples.card>
+                <h3>Definition</h3>
+                <div class="grid gap-2">
+                    <p>
+                        Links er klikbare tekst- eller iconelementer, der forbinder brugeren til en anden side, et
+                        dokument eller en ekstern ressource. De udgør den primære metode til at hoppe mellem indhold og
+                        funktioner i systemet.
+                    </p>
+                </div>
+            </x-expamples.card>
+            <x-expamples.card>
+                <h3>Funktionalitet</h3>
+                <div class="grid gap-2">
+                    <p>
+                        Links bruges til at navigere internt i applikationen, downloade filer eller åbne eksterne sider.
+                        De kan placeres i brødtekst, lister, kort eller knapper og aktiveres med ét klik eller tryk. Når
+                        brugeren holder musen over et link skifter det udseende for at vise, at elementet er
+                        interaktivt.
+                    </p>
+                </div>
+            </x-expamples.card>
+            <x-expamples.card>
+                <h3>Designprincipper</h3>
+                <div class="grid gap-2">
+                    <p>
+                        <span class="font-bold">Tydelighed:</span>
+                        Links skal skille sig ud fra almindelig tekst via farve eller understregning; effekten
+                        forstærkes ved hover.
+                    </p>
+                    <p>
+                        <span class="font-bold">Tydelighed:</span>
+                        Links skal skille sig ud fra almindelig tekst via farve eller understregning; effekten
+                        forstærkes ved hover.
+                    </p>
+                    <p>
+                        <span class="font-bold">Beskrivende tekst:</span>
+                        Link‑tekster skal klart angive destinationen (fx “Download rapport” i stedet for “Klik her”).
+                    </p>
+                </div>
+            </x-expamples.card>
+        </div>
+    </div>
 </x-layout>
