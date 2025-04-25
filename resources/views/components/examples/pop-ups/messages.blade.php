211713a4e40840fa5ddb393e226e6b0e8549637b
Lukas Pedersen <lukas@agilex.dk>
2025-04-25 10:14:18 +0200

    feat: messages examples

--- resources/views/components/examples/pop-ups/messages.blade.php
+++ resources/views/components/examples/pop-ups/messages.blade.php
@@ -1,9 +1,9 @@
 @php
     $iconComponent = "examples.icons.messages.$message";
 @endphp
-<div class="bg-{{ $message }} bg-opacity-10 text-{{ $message }} rounded-lg border-2 border-dashed border-{{ $message }} p-1">
-    <p class="text-center flex items-center justify-center gap-1">
-        <span class="message-icon"><x-dynamic-component :component="$iconComponent" /></span>
-        {{ $message }}
-    </p>
+<div class="bg-{{ $message }} bg-opacity-10 text-{{ $message }} rounded-lg border-2 border-{{ $message }} p-1 w-{{ $width ?? 'full' }}">
+    <div class="flex items-center gap-1">
+        <div class="message-icon"><x-dynamic-component :component="$iconComponent" /></div>
+        <p class="font-bold first-letter:uppercase">{{ $message }}!</p>
+    </div>
 </div>
