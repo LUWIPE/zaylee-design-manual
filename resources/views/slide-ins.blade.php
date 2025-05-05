e2f840cff43b14fbb21d95e8a6d82caede26f833
Lukas <lukaswp2710@gmail.com>
2025-05-05 11:42:04 +0200

    feat: slideins text

--- resources/views/slide-ins.blade.php
+++ resources/views/slide-ins.blade.php
@@ -7,19 +7,38 @@
             <x-examples.card.border>
                 <x-examples.card.body>
                     <h3>Definition</h3>
-                    <p></p>
+                    <p>
+                        Slide-ins er UI-elementer designet som paneler, der skjult ligger uden for skærmbilledet og
+                        glider synligt ind, når de aktiveres. De er en del af layoutets komponentstruktur og har en fast
+                        placering, som skaber en ensartet oplevelse for brugeren.
+                    </p>
                 </x-examples.card.body>
             </x-examples.card.border>
             <x-examples.card.border>
                 <x-examples.card.body>
                     <h3>Funktionalitet</h3>
-                    <p></p>
+                    <p>
+                        Slide-ins gør det muligt at interagere med ekstra indhold som formularer, detaljer, beskeder
+                        eller redigeringsmuligheder uden at navigere væk fra den side, brugeren allerede er på.
+                        Slide-ins kan åbnes og lukkes efter behov, og de er typisk koblet til specifikke handlinger som
+                        klik på en knap eller række i en tabel.
+                    </p>
                 </x-examples.card.body>
             </x-examples.card.border>
             <x-examples.card.border>
                 <x-examples.card.body>
                     <h3>Designprincipper</h3>
-                    <p></p>
+                    <p>
+                        <span class="font-bold">Kontrast</span>
+                        Skal have tydelig adskillelse fra baggrunden, f.eks. med skygge eller farvekontrast.
+                    </p>
+                    <p>
+                        <span class="font-bold">Animation:</span>
+                        Bør glide ind med en jævn og diskret animation for at undgå at forstyrre brugeren.
+                    </p>
+                    <p>
+                        <span class="font-bold"></span>
+                    </p>
                 </x-examples.card.body>
             </x-examples.card.border>
         </div>
