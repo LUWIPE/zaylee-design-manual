cd23a7ce6c7ed41a62ed74cef270c5193e3407be
Lukas <lukaswp2710@gmail.com>
2025-05-07 13:37:22 +0200

    feat: new page text + examples

---
+++ resources/views/code-blocks/new-page.blade.php
@@ -0,0 +1,8 @@
+&lt;x-layout>
+    &lt;div class="page-container relative h-full flex flex-auto
+                flex-col px-4 sm:px-6 py-4 sm:py-6 md:px-8 container mx-auto">
+        &lt;div class="container mx-auto h-full">
+            Indhold
+        &lt;/div>
+    &lt;/div>
+&lt;/x-layout>
