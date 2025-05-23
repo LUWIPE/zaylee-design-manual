6516cc4fa3f5bf56ab13047bc536e56d8d5393fe
Lukas Pedersen <lukas@agilex.dk>
2025-05-23 11:05:39 +0200

    feat: lists update

--- resources/views/code-blocks/lists.blade.php
+++ resources/views/code-blocks/lists.blade.php
@@ -1,8 +1,49 @@
 "lists.simple"
 Komponent
 &#64;props(['ths' => [], 'tds' => []])
-&lt;x-examples.card>
-    &lt;div class="tabs">
+&lt;div class="tabs">
+    &lt;div class="tab-content" role="tabpanel" tabindex="0">
+        &lt;div class="overflow-x-auto custom-scrollbar-dark">
+            &lt;table class="table-compact table-hover w-full">
+                &lt;thead>
+                &lt;tr class="table-row border-b">
+                    &#64;foreach($ths as $th)
+                        &lt;th>&#123;&#123; $th }}&lt;/th>
+                    &#64;endforeach
+                &lt;/tr>
+                &lt;/thead>
+                &lt;tbody>
+                &#64;foreach($tds as $row)
+                    &lt;tr class="table-row odd:bg-gray-50">
+                        &#64;foreach($row as $td)
+                            &lt;td class="align-middle">&#123;&#123; $td }}&lt;/td>
+                        &#64;endforeach
+                    &lt;/tr>
+                &#64;endforeach
+                &lt;/tbody>
+            &lt;/table>
+        &lt;/div>
+    &lt;/div>
+&lt;/div>
+
+"lists.heading"
+Komponent
+&#64;props(['ths' => [], 'tds' => []])
+&lt;div class="tabs">
+    &lt;div class="tab-list-underline" role="tablist">
+        &lt;div class="flex flex-row justify-between">
+            &lt;div class="col-span-1">
+                &lt;div class="tab-list">
+                    &lt;p class="tab-list-item active">
+                           &lt;span class="tab-nav tab-nav-underline text-info border-info">
+                               Overskrift
+                           &lt;/span>
+                    &lt;/p>
+                &lt;/div>
+            &lt;/div>
+        &lt;/div>
+    &lt;/div>
+    &lt;div class="p-4">
         &lt;div class="tab-content" role="tabpanel" tabindex="0">
             &lt;div class="overflow-x-auto custom-scrollbar-dark">
                 &lt;table class="table-compact table-hover w-full">
@@ -26,110 +67,85 @@ Komponent
             &lt;/div>
         &lt;/div>
     &lt;/div>
-&lt;/x-examples.card>
-
-"lists.heading"
-Komponent
-&#64;props(['ths' => [], 'tds' => []])
-&lt;x-examples.card>
-    &lt;div class="tabs">
-        &lt;div class="tab-list-underline" role="tablist">
-            &lt;div class="flex flex-row justify-between">
-                &lt;div class="col-span-1">
-                    &lt;div class="tab-list">
-                        &lt;p class="tab-list-item active">
-                               &lt;span class="tab-nav tab-nav-underline text-info border-info">
-                                   Overskrift
-                               &lt;/span>
-                        &lt;/p>
-                    &lt;/div>
-                &lt;/div>
-            &lt;/div>
-        &lt;/div>
-        &lt;div class="p-4">
-            &lt;div class="tab-content" role="tabpanel" tabindex="0">
-                &lt;div class="overflow-x-auto custom-scrollbar-dark">
-                    &lt;table class="table-compact table-hover w-full">
-                        &lt;thead>
-                        &lt;tr class="table-row border-b">
-                            &#64;foreach($ths as $th)
-                                &lt;th>&#123;&#123; $th }}&lt;/th>
-                            &#64;endforeach
-                        &lt;/tr>
-                        &lt;/thead>
-                        &lt;tbody>
-                        &#64;foreach($tds as $row)
-                            &lt;tr class="table-row odd:bg-gray-50">
-                                &#64;foreach($row as $td)
-                                    &lt;td class="align-middle">&#123;&#123; $td }}&lt;/td>
-                                &#64;endforeach
-                            &lt;/tr>
-                        &#64;endforeach
-                        &lt;/tbody>
-                    &lt;/table>
-                &lt;/div>
-            &lt;/div>
-        &lt;/div>
-    &lt;/div>
-&lt;/x-examples.card>
+&lt;/div>
 
 "lists.tabs"
 Komponent
-&lt;x-examples.card>
-    &lt;div
-        class="tabs"
-        x-data='{ tabs: &#64;json($tabs), activeTab: null }'
-        x-init="activeTab = tabs[0].name"
-    >
-        &lt;div class="tab-list-underline" role="tablist">
-            &lt;div class="flex flex-row justify-between">
-                &lt;div class="col-span-1">
-                    &lt;div
-                        class="tab-list"
-                    >
-                        &lt;template x-for="tab in tabs" x-bind:key="tab.name">
-                            &lt;p
-                                class="tab-nav tab-nav-underline"
-                                x-on:click="activeTab = tab.name"
-                                x-bind:class="activeTab === tab.name ? 'text-info border-info' : ''"
-                                x-text="tab.name"
-                            >
-                            &lt;/p>
-                        &lt;/template>
-                    &lt;/div>
-                &lt;/div>
-            &lt;/div>
-        &lt;/div>
-        &lt;div class="p-4">
-            &lt;div
-                class="tab-content"
-                role="tabpanel"
-                tabindex="0"
-            >
-                &lt;div class="overflow-x-auto custom-scrollbar-dark">
-                    &lt;table class="table-compact table-hover w-full">
-                        &lt;thead>
-                        &lt;tr class="table-row border-b">
-                            &lt;template x-for="heading in tabs.find(tab => tab.name === activeTab).headings"
-                                      x-bind:key="heading">
-                                &lt;th x-text="heading">&lt;/th>
-                            &lt;/template>
-                        &lt;/tr>
-                        &lt;/thead>
-                        &lt;tbody>
-                        &lt;template x-for="rows in tabs.find(tab => tab.name === activeTab).data">
-                            &lt;tr class="table-row odd:bg-gray-50">
-                                &lt;template x-for="col in rows" x-bind:key="col">
-                                    &lt;td x-text="col">&lt;/td>
-                                &lt;/template>
-                            &lt;/tr>
-                        &lt;/template>
-                        &lt;/tbody>
-                    &lt;/table>
+&lt;div
+    class="tabs"
+    x-data='&#123; tabs: &#64;json($tabs), activeTab: null }'
+    x-init="activeTab = tabs[0].name"
+>
+    &lt;div class="tab-list-underline" role="tablist">
+        &lt;div class="flex flex-row justify-between">
+            &lt;div class="col-span-1">
+                &lt;div
+                    class="tab-list"
+                >
+                    &lt;template x-for="tab in tabs" x-bind:key="tab.name">
+                        &lt;p
+                            class="tab-nav tab-nav-underline"
+                            x-on:click="activeTab = tab.name"
+                            x-bind:class="activeTab === tab.name ? 'text-primary border-primary' : ''"
+                            x-text="tab.name"
+                        >
+                        &lt;/p>
+                    &lt;/template>
                 &lt;/div>
             &lt;/div>
         &lt;/div>
     &lt;/div>
-&lt;/x-examples.card>
+    &lt;div class="p-4">
+        &lt;div
+            class="tab-content"
+            role="tabpanel"
+            tabindex="0"
+        >
+            &lt;div class="overflow-x-auto custom-scrollbar-dark">
+                &lt;table class="table-compact table-hover w-full">
+                    &lt;thead>
+                    &lt;tr class="table-row border-b">
+                        &lt;template x-for="heading in tabs.find(tab => tab.name === activeTab).headings" x-bind:key="heading">
+                            &lt;th class="text-nowrap uppercase" x-text="heading">&lt;/th>
+                        &lt;/template>
+                    &lt;/tr>
+                    &lt;/thead>
+                    &lt;tbody>
+                    &lt;template x-for="(row, rowIndex) in tabs.find(tab => tab.name === activeTab).data" x-bind:key="rowIndex">
+                        &lt;tr
+                            class="table-row border-b border-gray-100 h-14"
+                            x-data="&#123; hovering: false }"
+                            x-on:mouseenter="hovering = true"
+                            x-on:mouseleave="hovering = false"
+                        >
+                            &lt;template x-for="(col, colIndex) in row" x-bind:key="colIndex">
+                                &lt;td
+                                    class="align-middle cursor-pointer"
+                                    x-on:click="slideWithoutBackdrop = true"
+                                    x-text="col"
+                                >&lt;/td>
+                            &lt;/template>
+                            &lt;td class="w-24 align-middle">
+                                &lt;div class="flex justify-end" x-show="hovering" x-transition.opacity.duration.150ms x-cloak>
+                                    &lt;a href="#" title="Rediger">
+                                        &lt;div class="hover:bg-gray-300 rounded-full p-2 table-icon">
+                                            &lt;x-icons.edit/>
+                                        &lt;/div>
+                                    &lt;/a>
+                                    &lt;a href="#" title="Slet">
+                                        &lt;div class="hover:bg-gray-300 rounded-full p-2 table-icon">
+                                            &lt;x-icons.trashcan/>
+                                        &lt;/div>
+                                    &lt;/a>
+                                &lt;/div>
+                            &lt;/td>
+                        &lt;/tr>
+                    &lt;/template>
+                    &lt;/tbody>
+                &lt;/table>
+            &lt;/div>
+        &lt;/div>
+    &lt;/div>
+&lt;/div>
 
 
--- resources/views/components/examples/lists/tabs.blade.php
+++ resources/views/components/examples/lists/tabs.blade.php
@@ -1,21 +1,21 @@
 <x-examples.card>
     <div
-            class="tabs"
-            x-data='{ tabs: @json($tabs), activeTab: null }'
-            x-init="activeTab = tabs[0].name"
+        class="tabs"
+        x-data='{ tabs: @json($tabs), activeTab: null }'
+        x-init="activeTab = tabs[0].name"
     >
         <div class="tab-list-underline" role="tablist">
             <div class="flex flex-row justify-between">
                 <div class="col-span-1">
                     <div
-                            class="tab-list"
+                        class="tab-list"
                     >
                         <template x-for="tab in tabs" x-bind:key="tab.name">
                             <p
-                                    class="tab-nav tab-nav-underline"
-                                    x-on:click="activeTab = tab.name"
-                                    x-bind:class="activeTab === tab.name ? 'text-info border-info' : ''"
-                                    x-text="tab.name"
+                                class="tab-nav tab-nav-underline"
+                                x-on:click="activeTab = tab.name"
+                                x-bind:class="activeTab === tab.name ? 'text-primary border-primary' : ''"
+                                x-text="tab.name"
                             >
                             </p>
                         </template>
@@ -25,26 +25,48 @@
         </div>
         <div class="p-4">
             <div
-                    class="tab-content"
-                    role="tabpanel"
-                    tabindex="0"
+                class="tab-content"
+                role="tabpanel"
+                tabindex="0"
             >
                 <div class="overflow-x-auto custom-scrollbar-dark">
                     <table class="table-compact table-hover w-full">
                         <thead>
                         <tr class="table-row border-b">
-                            <template x-for="heading in tabs.find(tab => tab.name === activeTab).headings"
-                                      x-bind:key="heading">
-                                <th x-text="heading"></th>
+                            <template x-for="heading in tabs.find(tab => tab.name === activeTab).headings" x-bind:key="heading">
+                                <th class="text-nowrap uppercase" x-text="heading"></th>
                             </template>
                         </tr>
                         </thead>
                         <tbody>
-                        <template x-for="rows in tabs.find(tab => tab.name === activeTab).data">
-                            <tr class="table-row odd:bg-gray-50">
-                                <template x-for="col in rows" x-bind:key="col">
-                                    <td x-text="col"></td>
+                        <template x-for="(row, rowIndex) in tabs.find(tab => tab.name === activeTab).data" x-bind:key="rowIndex">
+                            <tr
+                                class="table-row border-b border-gray-100 h-14"
+                                x-data="{ hovering: false }"
+                                x-on:mouseenter="hovering = true"
+                                x-on:mouseleave="hovering = false"
+                            >
+                                <template x-for="(col, colIndex) in row" x-bind:key="colIndex">
+                                    <td
+                                        class="align-middle cursor-pointer"
+                                        x-on:click="slideWithoutBackdrop = true"
+                                        x-text="col"
+                                    ></td>
                                 </template>
+                                <td class="w-24 align-middle">
+                                    <div class="flex justify-end" x-show="hovering" x-transition.opacity.duration.150ms x-cloak>
+                                        <a href="#" title="Rediger">
+                                            <div class="hover:bg-gray-300 rounded-full p-2 table-icon">
+                                                <x-examples.icons.edit/>
+                                            </div>
+                                        </a>
+                                        <a href="#" title="Slet">
+                                            <div class="hover:bg-gray-300 rounded-full p-2 table-icon">
+                                                <x-examples.icons.trashcan/>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </td>
                             </tr>
                         </template>
                         </tbody>
