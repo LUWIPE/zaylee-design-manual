12f062faec1367ce3c78109eade7f13571ba3cdd
Lukas Pedersen <lukas@agilex.dk>
2025-05-26 11:43:31 +0200

    feat: tabbed index codeblock

--- resources/views/code-blocks/lists.blade.php
+++ resources/views/code-blocks/lists.blade.php
@@ -148,4 +148,101 @@ Komponent
     &lt;/div>
 &lt;/div>
 
+"lists.tabbed-index"
+Komponent
+&#64;props([
+    'indexData',
+])
+&#64;php
+    $currentTabKey = request('tab', $indexData['defaultTab']);
+    $currentTab = $indexData['tabs']->firstWhere('key', $currentTabKey);
+&#64;endphp
+&lt;div>
+    &lt;div class="flex justify-between items-center">
+        &lt;div class="tab-list-underline w-full" role="tablist">
+            &lt;div class="flex flex-row justify-between">
+                &lt;div class="col-span-1">
+                    &lt;div class="tab-list">
+                        &#64;foreach($indexData['tabs'] as $tab)
+                            &lt;a href="&#123;&#123; request()->fullUrlWithQuery(['tab' => $tab['key']]) }}">
+                                &#64;php $isActiveTab = $tab['key'] === $currentTabKey; &#64;endphp
+                                &lt;p &#64;class([
+                                                'tab-nav tab-nav-underline',
+                                                'text-primary border-primary' => $isActiveTab,
+                                                'hover:text-primary' => !$isActiveTab,
+                                            ])>
+                                    &#123;&#123; $tab['name'] }}
+                                &lt;/p>
+                            &lt;/a>
+                        &#64;endforeach
+                    &lt;/div>
+                &lt;/div>
+                &#64;if($indexData['canCreate'])
+                    &lt;a href="&#123;&#123; $indexData['getCreateUrl']() }}">
+                        &lt;x-examples.buttons.filled-sm
+                            color="primary"
+                            text="Opret Tilbud"
+                        />
+                    &lt;/a>
+                &#64;endif
+            &lt;/div>
+        &lt;/div>
+    &lt;/div>
+    &lt;div class="p-4">
+        &lt;div
+            class="tab-content"
+            role="tabpanel"
+            tabindex="0"
+        >
+            &lt;div class="overflow-x-auto custom-scrollbar-dark">
+                &lt;table class="table-compact table-hover w-full">
+                    &lt;thead>
+                    &lt;tr class="table-row border-b">
+                        &#64;foreach($currentTab['columns'] as $column)
+                            &lt;th class="text-nowrap uppercase">&#123;&#123; $column['heading'] }}&lt;/th>
+                        &#64;endforeach
+                    &lt;/tr>
+                    &lt;/thead>
+                    &lt;tbody>
+                    &#64;foreach($indexData['objects'] as $object)
+                        &lt;tr
+                            class="table-row border-b border-gray-100 h-14"
+                            x-data="&#123; hovering: false }"
+                            x-on:mouseenter="hovering = true"
+                            x-on:mouseleave="hovering = false"
+                        >
+                            &#64;foreach($currentTab['columns'] as $column)
+                                &lt;td
+                                    class="align-middle cursor-pointer"
+                                    x-on:click="slideWithoutBackdrop = true"
+                                >&#123;&#123; $column['parseValue']($object) }}&lt;/td>
+                            &#64;endforeach
+                            &lt;td class="w-24 align-middle">
+                                &lt;div
+                                    class="flex justify-end"
+                                    x-show="hovering"
+                                    x-transition.opacity.duration.300ms
+                                    x-cloak
+                                >
+                                    &lt;a href="#" title="Rediger">
+                                        &lt;div class="hover:bg-gray-300 rounded-full p-2 table-icon">
+                                            &lt;x-examples.icons.edit/>
+                                        &lt;/div>
+                                    &lt;/a>
+                                    &lt;a href="#" title="Slet">
+                                        &lt;div class="hover:bg-gray-300 rounded-full p-2 table-icon">
+                                            &lt;x-examples.icons.trashcan/>
+                                        &lt;/div>
+                                    &lt;/a>
+                                &lt;/div>
+                            &lt;/td>
+                        &lt;/tr>
+                    &#64;endforeach
+                    &lt;/tbody>
+                &lt;/table>
+                &lt;x-examples.pagination :page="$indexData['page']" :lastPage="$indexData['lastPage']" />
+            &lt;/div>
+        &lt;/div>
+    &lt;/div>
+&lt;/div>
 
